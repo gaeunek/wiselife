81ed47a09019227c5d3f50972839c90abb015c39
gaeunek <l9960204@naver.com>
2020-06-17 12:02:26 +0900

    200617 | Update

--- wiselife/src/main/java/com/ssafy/wiselife/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.wiselife.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ssafy.wiselife.domain.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer>{
-
-}
--- wiselife/src/main/java/com/ssafy/wiselife/service/IUserService.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.wiselife.service;
-
-import com.ssafy.wiselife.dto.UserDTO;
-
-public interface IUserService {
-	
-	int signUp(UserDTO user);
-	
-}
